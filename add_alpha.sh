60d683e268651e64a4b148ea6c2d6a46383f00dc
Eugene Vert <eugene.a.vert@gmail.com>
2021-07-05 02:43:14 +0300

    imagemagick script for adding aplha ch.

---
+++ add_alpha.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd "$1" || exit
+for i in *.png; do
+has_alpha=$(identify -format '%[channels]' "$i" | grep srgba)
+if [[ ! "$has_alpha" ]]; then
+    echo "$i"
+    mkdir -p ./orig
+    mv "$i" ./orig
+    convert ./orig/"$i" -alpha on "$i"
+fi
+done
